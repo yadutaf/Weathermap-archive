22eb817d173cc0caf9e09c3ea036fe118e0226e1
Jean-Tiare Le Bigot <jtlebi@gmail.com>
2012-04-09 17:49:01 +0200

    initial client application

---
+++ static/coffee-script/app.coffee
@@ -0,0 +1,196 @@
+Weathermaps = Ember.Application.create()
+
+baseurl = "http://test1.jtlebi.fr/wm-api"
+
+###
+Models/Controllers
+###
+
+Weathermaps.groups = Em.ArrayController.create {
+  value: ''
+  options: []
+  
+  refresh: (->
+    $.getJSON baseurl+"/groups", (data) =>
+      console.log data
+      @set 'options', data
+      #if we have only one value, auto-select it
+      if data.length is 1
+        @set 'value', data[0]
+  )
+}
+
+Weathermaps.maps = Em.ArrayController.create {
+  value: ''
+  options: []
+  groupBinding: 'Weathermaps.groups.value'
+  
+  refresh: (->
+    @set 'value', ""#on group change, reset active value
+    group = @get 'group'
+    if not group
+      @set 'options', []
+    else
+      $.getJSON baseurl+"/"+group+"/maps", (data) =>
+        console.log data
+        @set 'options', data
+        #if we have only one value, auto-select it
+        if data.length is 1
+          @set 'value', data[0]
+  ).observes("group")
+}
+
+Weathermaps.dates = Em.ArrayController.create {
+  value: ''
+  options: []
+  groupBinding: 'Weathermaps.groups.value'
+  mapBinding:   'Weathermaps.maps.value'
+  
+  refresh: (->
+    @set 'value', ""#on map change, reset active value
+    group = @get 'group'
+    map = @get 'map'
+    if not map
+      @set 'options', []
+    else
+      $.getJSON baseurl+"/"+group+"/"+map+"/dates", (data) =>
+        console.log data
+        data.sort()
+        data.reverse()
+        @set 'options', data
+        #auto-select the most recent
+        if data.length > 1
+          @set 'value', data[0]
+  ).observes("map")
+}
+
+Weathermaps.times = Em.ArrayController.create {
+  value: ''
+  options: []
+  groupBinding: 'Weathermaps.groups.value'
+  mapBinding:   'Weathermaps.maps.value'
+  dateBinding:  'Weathermaps.dates.value'
+  
+  refresh: (->
+    @set 'value', ""#on map change, reset active value
+    group = @get 'group'
+    map = @get 'map'
+    date = @get 'date'
+    if not date
+      @set 'options', []
+    else
+      $.getJSON baseurl+"/"+group+"/"+map+"/"+date+"/times", (data) =>
+        console.log data
+        data.sort()
+        data.reverse()
+        @set 'options', data
+        #auto-select the most recent
+        if data.length > 1
+          @set 'value', data[0]
+  ).observes("date")
+}
+
+Weathermaps.current = Ember.Object.create {
+  groupBinding: "Weathermaps.groups.value"
+  mapBinding: "Weathermaps.maps.value"
+  dateBinding: "Weathermaps.dates.value"
+  timeBinding: "Weathermaps.times.value"
+  url: (->
+    group = @get('group')
+    map = @get('map')
+    date = @get('date')
+    time = @get('time')
+    if group and map and date and time
+      "http://ovh.jtlebi.fr/"+group+"/"+map+"/"+date+"/"+time+".png"
+     else
+      ""
+  ).property('group', 'map', 'date', 'time')
+}
+
+###
+Views
+###
+
+Weathermaps.GroupListView = Ember.View.extend {
+  templateName: 'grouplist'
+  active: true #always visible
+  
+  valueBinding: 'Weathermaps.groups.value'
+  optionsBinding: 'Weathermaps.groups.options'
+  
+  title: (->
+    value = @get 'value'
+    if value then value else 'Group name'
+  ).property 'value'
+  
+  select: (e) ->
+    @set 'value', e.context
+  
+}
+
+Weathermaps.MapListView = Ember.View.extend {
+  templateName: 'maplist'
+  groupBinding: 'Weathermaps.groups.value'
+  active: (->
+    return if @get('group').length then true else false
+   ).property 'group'
+  
+  valueBinding: 'Weathermaps.maps.value'
+  optionsBinding: 'Weathermaps.maps.options'
+  
+  title: (->
+    value = @get 'value'
+    if value then value else 'Map name'
+  ).property('value')
+  
+  select: (e) ->
+    @set 'value', e.context
+  
+}
+
+Weathermaps.DateListView = Ember.View.extend {
+  templateName: 'datelist'
+  groupBinding: 'Weathermaps.groups.value'
+  mapBinding:   'Weathermaps.maps.value'
+  active: (->
+    return if @get('map').length then true else false
+   ).property 'map'
+  
+  valueBinding: 'Weathermaps.dates.value'
+  optionsBinding: 'Weathermaps.dates.options'
+  
+  title: (->
+    value = @get 'value'
+    if value then value else 'Date'
+  ).property('value')
+  
+  select: (e) ->
+    @set 'value', e.context
+  
+}
+
+Weathermaps.TimeListView = Ember.View.extend {
+  templateName: 'datelist'
+  groupBinding: 'Weathermaps.groups.value'
+  mapBinding:   'Weathermaps.maps.value'
+  dateBinding:  'Weathermaps.dates.value'
+  active: (->
+    return if @get('date').length then true else false
+   ).property 'date'
+  
+  valueBinding: 'Weathermaps.times.value'
+  optionsBinding: 'Weathermaps.times.options'
+  
+  title: (->
+    value = @get 'value'
+    if value then value else 'Time'
+  ).property('value')
+  
+  select: (e) ->
+    @set 'value', e.context
+  
+}
+
+#init
+Weathermaps.groups.refresh()
+
---
+++ static/coffee-script/app.js
@@ -0,0 +1,220 @@
+var Weathermaps, baseurl;
+
+Weathermaps = Ember.Application.create();
+
+baseurl = "http://test1.jtlebi.fr/wm-api";
+
+/*
+Models/Controllers
+*/
+
+Weathermaps.groups = Em.ArrayController.create({
+  value: '',
+  options: [],
+  refresh: (function() {
+    var _this = this;
+    return $.getJSON(baseurl + "/groups", function(data) {
+      console.log(data);
+      _this.set('options', data);
+      if (data.length === 1) return _this.set('value', data[0]);
+    });
+  })
+});
+
+Weathermaps.maps = Em.ArrayController.create({
+  value: '',
+  options: [],
+  groupBinding: 'Weathermaps.groups.value',
+  refresh: (function() {
+    var group,
+      _this = this;
+    this.set('value', "");
+    group = this.get('group');
+    if (!group) {
+      return this.set('options', []);
+    } else {
+      return $.getJSON(baseurl + "/" + group + "/maps", function(data) {
+        console.log(data);
+        _this.set('options', data);
+        if (data.length === 1) return _this.set('value', data[0]);
+      });
+    }
+  }).observes("group")
+});
+
+Weathermaps.dates = Em.ArrayController.create({
+  value: '',
+  options: [],
+  groupBinding: 'Weathermaps.groups.value',
+  mapBinding: 'Weathermaps.maps.value',
+  refresh: (function() {
+    var group, map,
+      _this = this;
+    this.set('value', "");
+    group = this.get('group');
+    map = this.get('map');
+    if (!map) {
+      return this.set('options', []);
+    } else {
+      return $.getJSON(baseurl + "/" + group + "/" + map + "/dates", function(data) {
+        console.log(data);
+        data.sort();
+        data.reverse();
+        _this.set('options', data);
+        if (data.length > 1) return _this.set('value', data[0]);
+      });
+    }
+  }).observes("map")
+});
+
+Weathermaps.times = Em.ArrayController.create({
+  value: '',
+  options: [],
+  groupBinding: 'Weathermaps.groups.value',
+  mapBinding: 'Weathermaps.maps.value',
+  dateBinding: 'Weathermaps.dates.value',
+  refresh: (function() {
+    var date, group, map,
+      _this = this;
+    this.set('value', "");
+    group = this.get('group');
+    map = this.get('map');
+    date = this.get('date');
+    if (!date) {
+      return this.set('options', []);
+    } else {
+      return $.getJSON(baseurl + "/" + group + "/" + map + "/" + date + "/times", function(data) {
+        console.log(data);
+        data.sort();
+        data.reverse();
+        _this.set('options', data);
+        if (data.length > 1) return _this.set('value', data[0]);
+      });
+    }
+  }).observes("date")
+});
+
+Weathermaps.current = Ember.Object.create({
+  groupBinding: "Weathermaps.groups.value",
+  mapBinding: "Weathermaps.maps.value",
+  dateBinding: "Weathermaps.dates.value",
+  timeBinding: "Weathermaps.times.value",
+  url: (function() {
+    var date, group, map, time;
+    group = this.get('group');
+    map = this.get('map');
+    date = this.get('date');
+    time = this.get('time');
+    if (group && map && date && time) {
+      return "http://ovh.jtlebi.fr/" + group + "/" + map + "/" + date + "/" + time + ".png";
+    } else {
+      return "";
+    }
+  }).property('group', 'map', 'date', 'time')
+});
+
+/*
+Views
+*/
+
+Weathermaps.GroupListView = Ember.View.extend({
+  templateName: 'grouplist',
+  active: true,
+  valueBinding: 'Weathermaps.groups.value',
+  optionsBinding: 'Weathermaps.groups.options',
+  title: (function() {
+    var value;
+    value = this.get('value');
+    if (value) {
+      return value;
+    } else {
+      return 'Group name';
+    }
+  }).property('value'),
+  select: function(e) {
+    return this.set('value', e.context);
+  }
+});
+
+Weathermaps.MapListView = Ember.View.extend({
+  templateName: 'maplist',
+  groupBinding: 'Weathermaps.groups.value',
+  active: (function() {
+    if (this.get('group').length) {
+      return true;
+    } else {
+      return false;
+    }
+  }).property('group'),
+  valueBinding: 'Weathermaps.maps.value',
+  optionsBinding: 'Weathermaps.maps.options',
+  title: (function() {
+    var value;
+    value = this.get('value');
+    if (value) {
+      return value;
+    } else {
+      return 'Map name';
+    }
+  }).property('value'),
+  select: function(e) {
+    return this.set('value', e.context);
+  }
+});
+
+Weathermaps.DateListView = Ember.View.extend({
+  templateName: 'datelist',
+  groupBinding: 'Weathermaps.groups.value',
+  mapBinding: 'Weathermaps.maps.value',
+  active: (function() {
+    if (this.get('map').length) {
+      return true;
+    } else {
+      return false;
+    }
+  }).property('map'),
+  valueBinding: 'Weathermaps.dates.value',
+  optionsBinding: 'Weathermaps.dates.options',
+  title: (function() {
+    var value;
+    value = this.get('value');
+    if (value) {
+      return value;
+    } else {
+      return 'Date';
+    }
+  }).property('value'),
+  select: function(e) {
+    return this.set('value', e.context);
+  }
+});
+
+Weathermaps.TimeListView = Ember.View.extend({
+  templateName: 'datelist',
+  groupBinding: 'Weathermaps.groups.value',
+  mapBinding: 'Weathermaps.maps.value',
+  dateBinding: 'Weathermaps.dates.value',
+  active: (function() {
+    if (this.get('date').length) {
+      return true;
+    } else {
+      return false;
+    }
+  }).property('date'),
+  valueBinding: 'Weathermaps.times.value',
+  optionsBinding: 'Weathermaps.times.options',
+  title: (function() {
+    var value;
+    value = this.get('value');
+    if (value) {
+      return value;
+    } else {
+      return 'Time';
+    }
+  }).property('value'),
+  select: function(e) {
+    return this.set('value', e.context);
+  }
+});
+
+Weathermaps.groups.refresh();
